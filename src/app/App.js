669ca436fbdd6d69afc022d871d9765576691d32
Denys Fesenko <dfesuk@gmail.com>
2022-01-01 19:36:00 +0200

    create button

--- src/app/App.js
+++ src/app/App.js
@@ -1,11 +1,12 @@
-import logo from '../resources/logo.svg';
+// import logo from '../resources/logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <button className='button-circle'>+</button>
+        {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -16,7 +17,7 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
