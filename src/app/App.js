3d2023d7a04213c5c085f2277aca32135b677fe8
Denys Fesenko <dfesuk@gmail.com>
2022-01-02 20:33:57 +0200

    add functional for btn & add btn of reset

--- src/app/App.js
+++ src/app/App.js
@@ -1,26 +1,48 @@
 // import logo from '../resources/logo.svg';
+import redo from '../resources/redo.png'
+import { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <button className='button-circle'>+</button>
-        {/* <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
-      </header>
-    </div>
-  );
+class ReadingNumbers extends Component {
+  state = {
+    number: 0
+  }
+  
+  changeNumber = () => {
+    let number = ++this.state.number
+    this.setState({
+      number
+    });
+  }
+
+  resetNumber = () => {
+    this.setState({
+      number: 0
+    })
+  }
+
+  
+  render() {
+    const {number} = this.state;
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <button 
+            className='button-circle'
+            onClick={this.changeNumber}
+          >+</button>
+          <img 
+            src={redo} 
+            alt="reset" 
+            className='btn-reset' 
+            onClick={this.resetNumber}
+          />
+          <p className='resault-number'>{number}</p>
+        </header>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default ReadingNumbers;
