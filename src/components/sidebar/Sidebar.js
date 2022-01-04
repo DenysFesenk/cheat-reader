1dca018603dd9bd834ede7d0d0189f939ca13541
Denys Fesenko <dfesuk@gmail.com>
2022-01-04 19:59:52 +0200

    create sidebar

---
+++ src/components/sidebar/Sidebar.js
@@ -0,0 +1,19 @@
+import { Component } from "react";
+import './Sidebar.scss';
+
+class Sidebar extends Component {
+    render() {
+        return(
+            <div className="sidebar">
+                <h1 className="sidebar__title">My App: </h1>
+                <ul className="sidebar__list">
+                    <li className="sidebar__item">Набиралка</li>
+                    <li className="sidebar__item">Калькулятор</li>
+                    <li className="sidebar__item">Конвертер валют</li>
+                </ul>
+            </div>
+        )
+    }
+}
+
+export default Sidebar;
