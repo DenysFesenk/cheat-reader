2358fed5b91469e1897a8dcebb83c63fef8fb291
Denys Fesenko <dfesuk@gmail.com>
2022-01-06 22:42:24 +0200

    work with router

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,15 +1,31 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 import { Component } from 'react';
 import GameOfNumbers from '../gameOfNumbers/GameOfNumbers';
 import Sidebar from '../sidebar/Sidebar';
 import './App.scss';
+import Calculator from '../calculator/Calculator';
+import Changer from '../changer/Changer';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Sidebar />
-        <GameOfNumbers />
-      </div>
+      <Router>
+        <div className="App">
+          <Sidebar />
+          <Switch>
+          <Route path='/'>
+            <GameOfNumbers />
+          </Route>
+          <Route path='/calculator'>
+            <Calculator />
+          </Route>
+          <Route path='/changer'>
+            <Changer />
+          </Route>
+          </Switch>
+        </div>
+      </Router>
     )
   }
 }
