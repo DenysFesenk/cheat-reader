f96a61a9214a3d6a5a3f33a71ff14610f07bd015
Denys Fesenko <dfesuk@gmail.com>
2022-01-06 22:41:48 +0200

    create two window for calc and changer

---
+++ src/components/changer/Changer.js
@@ -0,0 +1,11 @@
+import { Component } from "react";
+
+class Changer extends Component{
+    render() {
+        return(
+            <h1>Changer</h1>
+        )
+    }
+}
+
+export default Changer;
